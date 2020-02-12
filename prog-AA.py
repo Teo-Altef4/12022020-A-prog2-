b82d2c70ad38074a2f9bbe1a23c24b654bc65ad8
juninhocb <teodorofreiberger@gmail.com>
2020-02-12 10:23:49 -0300

    Ameno Omenari Imperavi Ameno

--- prog-AA.py
+++ prog-AA.py
@@ -1,1 +1,18 @@
-print ("hello world, im hungry...")
+def AMENO- ():
+    a1 = "Dorime"
+    a2 = "Ameno"
+    a3 = "Interino"
+    a4 = "Adapare"
+    a5 = "Latire, Latiremo"
+    a6 = "Omenari Imperavi"
+    a7 = "Diemere"
+    a8 = "Matire, Matiremo"
+    a9 = "Dore"
+
+def DORIME- ()
+ print ("a1 = Dorime a2 = Ameno a3 = Interino a4 = Adapare a5 = Latire, Latiremo a6 = Omenari Imperavi a7 = Diemere  a8 = Matire, Matiremo  a9 = Dore")
+       
+        
+        
+        
+        
