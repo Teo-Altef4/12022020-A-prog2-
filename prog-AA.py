eb50d6d16e8b8d6dfaa0b75678c3c2e5f31efb55
dfssf <dfgdfgdg>
2020-02-19 08:52:56 -0300

    Unga bUngA

--- prog-AA.py
+++ prog-AA.py
@@ -8,6 +8,7 @@ def AMENO- ():
     a7 = "Diemere"
     a8 = "Matire, Matiremo"
     a9 = "Dore"
+    #za waldo
 
 def DORIME- ()
  print ("a1 = Dorime a2 = Ameno a3 = Interino a4 = Adapare a5 = Latire, Latiremo a6 = Omenari Imperavi a7 = Diemere  a8 = Matire, Matiremo  a9 = Dore")
@@ -15,4 +16,4 @@ def DORIME- ()
         
         
         
-        
+#ooga booga        
